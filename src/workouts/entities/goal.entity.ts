5f738f775c9fcae961463162c238e64a43074fb6
Malky-Rubinstein <m0583253068@gmail.com>
2026-06-02 20:28:43 +0300

    Create goal.entity.ts

---
+++ src/workouts/entities/goal.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity('goals')
+export class Goal {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userId: number;
+
+  @Column({ type: 'varchar' }) // 'DAILY' או 'WEEKLY'
+  timeFrame: string;
+
+  @Column({ type: 'date' })
+  startDate: string;
+
+  @Column({ type: 'date' })
+  endDate: string;
+
+  @Column({ type: 'varchar' }) // 'CALORIES' או 'DURATION'
+  goalType: string;
+
+  @Column({ type: 'float' })
+  targetValue: number;
+
+  @Column({ type: 'float', default: 0 })
+  currentProgress: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  user: User;
+}
