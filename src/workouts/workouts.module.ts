a19b8ee4cf621b89ec6c5103f3fa1eb6638c8956
Malky-Rubinstein <m0583253068@gmail.com>
2026-06-02 20:29:50 +0300

    Update WorkoutsModule to include Goal entity

--- src/workouts/workouts.module.ts
+++ src/workouts/workouts.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { WorkoutsController } from './workouts.controller';
 import { WorkoutsService } from './workouts.service';
+import { WorkoutsController } from './workouts.controller';
 import { Workout } from './workout.entity';
-import { User } from '../users/entities/user.entity'; // <-- הנתיב המדויק והמתוקן!
+import { Goal } from './entities/goal.entity'; // הייבוא החדש
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Workout, User])],
+  imports: [TypeOrmModule: [Workout, Goal]], // הוספת Goal לכאן
   controllers: [WorkoutsController],
   providers: [WorkoutsService],
+  exports: [WorkoutsService],
 })
 export class WorkoutsModule {}
