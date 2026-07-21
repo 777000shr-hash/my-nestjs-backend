d7d30f3a329473e02cf2956bc58060afd45868fd
777000shr-hash <777000shr@gmail.com>
2026-07-21 13:14:07 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,6 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get('api/ping')
   getPing() {
     return { status: 'ok' };
