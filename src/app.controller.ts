51a8ee96d308839ee708ef612008802eb164e944
777000shr-hash <777000shr@gmail.com>
2026-07-21 12:56:12 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,12 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('api/ping')
   getPing() {
     return { status: 'ok' };
-  }
+  }  
 }
