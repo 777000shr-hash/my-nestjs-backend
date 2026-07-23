b12693c1f47210f32c92d2b7d20d53a6f354ffd1
Malky-Rubinstein <m0583253068@gmail.com>
2026-07-23 15:57:20 +0300

    Import User entity in workouts.module.ts

--- src/workouts/workouts.module.ts
+++ src/workouts/workouts.module.ts
@@ -4,10 +4,10 @@ import { WorkoutsService } from './workouts.service';
 import { WorkoutsController } from './workouts.controller';
 import { Workout } from './workout.entity';
 import { Goal } from './entities/goal.entity';
-import { User } from '../users/entities/user.entity'; // הייבוא של User
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Workout, Goal, User])], // הוספנו את User לכאן
+  imports: [TypeOrmModule.forFeature([Workout, Goal, User])],
   controllers: [WorkoutsController],
   providers: [WorkoutsService],
   exports: [WorkoutsService],
