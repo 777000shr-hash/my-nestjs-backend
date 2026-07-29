20d59bf5029869d123587ae1e540d2f581ac2742
Malky Levi <m0583253068@gmail.com>
2026-07-29 14:18:32 +0300

    Update reset-password.dto.ts

--- src/users/dto/reset-password.dto.ts
+++ src/users/dto/reset-password.dto.ts
@@ -5,9 +5,9 @@ export class ResetPasswordDto {
   email!: string;
 
   @IsString()
-  code!: string; // זה שדה ייחודי לאיפוס
+  code!: string; // This is a unique field to reset.
 
   @IsString()
   @MinLength(6, { message: 'Password must contain at least 6 characters' })
-  newPassword!: string; // זה השדה המקביל לסיסמה
+  newPassword!: string; // This is the field corresponding to the password.
 }
